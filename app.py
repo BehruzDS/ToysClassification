ba1e9641d1b42c1c595a8f840c04cf5514dd03b4
Behruz DS <109086672+BehruzDS@users.noreply.github.com>
2022-08-23 23:37:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import streamlit as st
 from fastai.vision.all import *
 import plotly.express as px
 import pathlib
+import platform
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
-
+plt = platform.system()
+if plt = 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
 # title
 st.title("O`yinchoqlarni klassifikatsiya qiladigan model")
